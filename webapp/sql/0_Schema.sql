e8fe8affa1652237295fb7a67dd7e1f6bdc0e023
sh-lu0 <chiaki.m.515@gmail.com>
2021-08-29 12:06:48 +0900

    isu_conditionテーブルにINDEX追加

--- webapp/sql/0_Schema.sql
+++ webapp/sql/0_Schema.sql
@@ -27,6 +27,8 @@ CREATE TABLE `isu_condition` (
   `condition` VARCHAR(255) NOT NULL,
   `message` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+  INDEX idx_jia_isu_uuid_timestamp (`jia_isu_uuid`, `timestamp`),
+  INDEX idx_jia_isu_uuid_timestamp_condition (`jia_isu_uuid`, `timestamp`, `condition`),
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
